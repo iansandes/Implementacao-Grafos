a674a8004c46269ce18beb32eac26dea27918bb3
Luan Vasconcelos <luanvaf@uol.com.br>
2020-03-11 09:23:08 -0300

    Busca em profundidade e eHRegular

--- Grafo.py
+++ Grafo.py
@@ -23,12 +23,24 @@ class Grafo(object):
                 vertices.append(vertice)
             return vertices
 
+    def ehRegular(self):
+        count = 0
+        for vertice in self.grafo.keys():
+            grauAtual = len(self.get_adjacentes(vertice))
+            if(count > 0 and (grauAtual != grauAntigo)):
+                return False
+            grauAntigo = grauAtual
+            count += 1
+        return True
+
     def eh_completo(self):
         
         qtd_arestas = 0
         qtd_vertices = len(self.grafo)
         for aresta in self.grafo.values():
             qtd_arestas += len(aresta)
+
+
                    
         calculo_grafo_completo = (qtd_vertices * (qtd_vertices - 1))/2
         
@@ -36,11 +48,26 @@ class Grafo(object):
             return True
         
         return False
-            
-            
-            
-            
 
-            
+    def grau(self, vertice):
+        grau = 0
+        for grau in range(len(self.get_adjacentes(vertice))):
+            grau += 1
+        return grau
+
+    def dfs(self): #busca em profundidade
+        def dfs_recursiva(self, vertice):
+            visitados.add(vertice)
+            for vizinho in self.grafo[vertice]:
+                if vizinho not in visitados:
+                    dfs_recursiva(self, vizinho)
+
+        visitados = set()
+        for vertice in self.grafo:
+            if not vertice in visitados:
+                dfs_recursiva(self, vertice)
+        return visitados
+
+
     def __str__(self):
         return f' {self.grafo}'
